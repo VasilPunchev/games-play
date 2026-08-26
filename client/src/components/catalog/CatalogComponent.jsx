cf8cba3cb64935ec5b5db1e4b4e9ca82ee73d296
Vasil Punchev <vpunchev85@gmail.com>
2026-08-26 20:35:55 +0300

    Render catalog games from server

--- client/src/components/catalog/CatalogComponent.jsx
+++ client/src/components/catalog/CatalogComponent.jsx
@@ -1,52 +1,38 @@
-export default function CatalogComponent () {
-    return(
-        <section id="catalog-page">
-  <h1>Catalog</h1>
-  {/* Display div: with information about every game (if any) */}
-  <div className="catalog-container">
-    <div className="game">
-      <img src="./images/witcher.png" alt="The Witcher 3" />
-      <div className="details-overlay">
-        <p className="name">The Witcher 3</p>
-        <p className="genre">Open World</p>
-        <a href="#" className="details-button">
-          Details
-        </a>
+import { useEffect, useState } from "react"
+import { getAll } from "../../services/gameService";
+import { Link } from "react-router-dom";
+
+export default function CatalogComponent() {
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    getAll().then(result => {
+      setGames(result)
+    })
+  }, [])
+  return (
+    <section id="catalog-page">
+      <h1>Catalog</h1>
+      <div className="catalog-container">
+        {games.map(game => (
+          <div className="game" key={game._id}>
+            <img src={game.imageUrl} alt={game.title} />
+            <div className="details-overlay">
+              <p className="name">{game.title}</p>
+              <p className="genre">{game.genre}</p>
+              <Link to={`/details/${game._id}`}
+                className="details-button">
+                Details
+              </Link>
+            </div>
+          </div>
+        ))}
+
+
       </div>
-    </div>
-    <div className="game">
-      <img src="./images/elden ring.png" alt="Elden Ring" />
-      <div className="details-overlay">
-        <p className="name">Elden Ring</p>
-        <p className="genre">Action RPG</p>
-        <a href="#" className="details-button">
-          Details
-        </a>
-      </div>
-    </div>
-    <div className="game">
-      <img src="./images/minecraft.png" alt="Minecraft" />
-      <div className="details-overlay">
-        <p className="name">Minecraft</p>
-        <p className="genre">Sandbox</p>
-        <a href="#" className="details-button">
-          Details
-        </a>
-      </div>
-    </div>
-    <div className="game">
-      <img src="./images/cyberpunk.png" alt="Cyberpunk 2077" />
-      <div className="details-overlay">
-        <p className="name">Cyberpunk 2077</p>
-        <p className="genre">Action RPG</p>
-        <a href="#" className="details-button">
-          Details
-        </a>
-      </div>
-    </div>
-  </div>
-  {/* Display paragraph: If there is no games  */}
-  {/* <h3 class="no-articles">No Added Games Yet</h3> */}
-</section>
-    )
+      {games.length === 0 && (
+        <h3 className ="no-articles">No Added Games Yet</h3>
+      )}
+
+    </section>
+  )
 }
