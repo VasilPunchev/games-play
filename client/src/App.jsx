c629cf077af8cfb66b80d673cc00e22771fce6a2
Vasil Punchev <vpunchev85@gmail.com>
2026-08-26 14:41:59 +0300

    Add initial React components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+
+import FooterComponent from './components/footer/FooterComponent'
+import HeaderComponent from './components/header/HeaderComponent'
+import HomeComponent from './components/home/HomeComponent'
+
 
 export default function App() {
 
 
   return (
     <>
+       <HeaderComponent/>
+       <HomeComponent/>
       
+       <FooterComponent/>
     </>
   )
 }
---
+++ client/src/components/footer/FooterComponent.jsx
@@ -0,0 +1,9 @@
+export default function FooterComponent () {
+    return (
+        
+            <footer>
+                <p className="center-text">React Course Exercise @ SoftUni &trade;</p>
+            </footer>
+      
+    )
+}
