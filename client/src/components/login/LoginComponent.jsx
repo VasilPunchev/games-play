8891974d8f91785d9c580e13326e9e536d2bff8b
Vasil Punchev <vpunchev85@gmail.com>
2026-09-03 20:53:27 +0300

    Add login functionality

--- client/src/components/login/LoginComponent.jsx
+++ client/src/components/login/LoginComponent.jsx
@@ -1,19 +1,47 @@
+import { useState } from "react"
+import { login } from "../../services/authService"
+import { useNavigate } from "react-router-dom"
 export default function LoginComponent() {
+ const [email, setEmail] = useState('');
+ const [password, setPassword] = useState('');
+
+ const navigate = useNavigate()
+
+ async function submitHandler(e) {
+    e.preventDefault()
+
+    try {
+        const result = await login(email, password)
+        localStorage.setItem('accessToken' , result.accessToken)
+        localStorage.setItem('userId', result._id)
+        navigate('/')
+        
+    } catch (err) {
+        console.error(err.message)
+    }
+ }
     return (
         <section id="login-page">
-            <form id="login">
+            <form id="login" onSubmit={submitHandler}>
                 <div className="container">
                     <h1>Login</h1>
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="Your Email" />
+                    <input type="email"
+                     id="email"
+                     name="email"
+                     value={email}
+                     onChange={(e)=> setEmail(e.target.value)}
+                     placeholder="Your Email" />
                     <label htmlFor="login-pass">Password</label>
                     <input
                         type="password"
                         id="login-password"
                         name="password"
                         placeholder="Password"
+                        value={password}
+                        onChange={(e)=> setPassword(e.target.value)}
                     />
-                    <input type="submit" className="btn submit" defaultValue="Login" />
+                    <input type="submit" className="btn submit" value="Login" />
                 </div>
             </form>
         </section>
