bdebf91a0fb3888c1e0ea91b70c747b5e72fc732
Vasil Punchev <vpunchev85@gmail.com>
2026-09-03 21:00:50 +0300

    Add register service

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -16,3 +16,18 @@ export async function login(email, password) {
     }
     return result;
 }
+
+export async function register(email, password) {
+    const response = await fetch(`${baseUrl}/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email, password})
+    })
+    const result = await response.json()
+    if (!response.ok) {
+        throw new Error(result.message)
+    }
+    return result;
+}
