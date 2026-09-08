6ec2f1e63555307c4c236081e9f2f4b57c7c6f16
Vasil Punchev <vpunchev85@gmail.com>
2026-09-08 18:26:18 +0300

    Implement service for loading game comments

---
+++ client/src/services/commentService.js
@@ -0,0 +1,16 @@
+const baseUrl = 'http://localhost:3030/jsonstore/comments'
+
+export async function getComments(gameId, signal) {
+    const response = await fetch (
+    `${baseUrl}?where=gameId%3D%22${gameId}%22`,
+     { signal }
+    )
+    const result = await response.json()
+    if (!response.ok) {
+        throw new Error(result.message)
+    }
+    return Object.entries(result).map(([id, comment]) => ({
+        ...comment,
+        _id: id
+    }))
+}
