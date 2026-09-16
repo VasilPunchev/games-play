44df51b82960d449481832cd1d178a98a7ebdaa1
Vasil Punchev <vpunchev85@gmail.com>
2026-09-16 18:36:04 +0300

    Wrap application with AuthProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
 import './styles/style.css'
+import AuthProvider from './contexts/AuthContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
+  
   <BrowserRouter>
+    <AuthProvider>
     <App />
+    </AuthProvider>
     </BrowserRouter>
+   
   
 )
